6ca171dc687f44bd2e505f647211532ca313e94a
rizface <alfarizzi2711@gmail.com>
2024-04-18 00:17:53 +0700

    handle when resp.data is null

--- script.mjs
+++ script.mjs
@@ -109,12 +109,12 @@ async function fetchData(date = null, city = null) {
         url: url,
     })  
 
-    // since the api never added a location, we will only process the data if the locations array is empty
-    if (locations.length == 0 )  {
-        extractLocations(resp.data)
-    }
-
     if (resp.data) {
+        // since the api never added a location, we will only process the data if the locations array is empty
+        if (locations.length == 0 )  {
+            extractLocations(resp.data)
+        }
+    
         forecast = [];
 
         extractForecast(resp.data, city)
